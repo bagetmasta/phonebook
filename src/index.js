a31f409ac06675a5581d64fcad1fd0e8df642e31
Artem Bahmet <baget.masta@gmail.com>
2023-07-18 09:16:19 +0300

    delete basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="react-phonebook">
+        <BrowserRouter>
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
